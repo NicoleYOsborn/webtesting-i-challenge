a56f563c2b10d5cb175e889a07b90a81a317debc
Nicole Osborn <nicole.y.osborn@gmail.com>
2020-08-11 13:21:57 -0700

    wrote todos for all tests

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,20 @@
 const enhancer = require('./enhancer.js');
+const { TestScheduler } = require('jest');
+const { success, fail, repair, get } = require("./enhancer.js");
 // test away!
+
+describe('enhancer functions', ()=>{
+    describe('success tests', ()=>{
+        test.todo('that it increases enhancement by 1');
+        test.todo('that id does not change if enhancment is 20');
+        test.todo('that durability is not changed');
+    })
+    describe('fail functions', ()=>{
+        test.todo('that it decreases durability by 5 if enhancement is < 15');
+        test.todo('that it decreases durability by 10 if enhancement is >= 15');
+        test.todo('that it decreases enhancement by 1 if enhancement leveli is > 16');
+    })
+    describe('repair functions', ()=>{
+        test.todo('that it returns a new item with durability of 100');
+    })
+})
